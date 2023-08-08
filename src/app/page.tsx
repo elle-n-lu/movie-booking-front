fee2dc2ea48b2b7ab10190e4c8f26b8956e4871d
elle-n-lu <joew1960938924@gmail.com>
2023-08-08 23:56:19 +1000

    add pages fetch movie session info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 "use client"
 import Movie from '@/movies/page'
 import Nav from '@/nav/page'
-import Schedules from '@/schedules/schedules'
+import Schedule from '@/schedules/page'
+import Session from '@/sessions/page'
 import Image from 'next/image'
 import { useState } from 'react'
 
 export default function Home() {
   const [cinemeId,setCinemaId]=useState(1)
+  const [schedules, setSchedules]=useState<any[]>([])
+  const [sessions, setSessions]=useState<any[]>([])
+  const [item, setItem] = useState(0);
+  // console.log('sched',schedules)
   return (
     <div >
-      <Nav cinemaId={cinemeId} setCinemaId={setCinemaId} />
-      <Movie cinemaId={cinemeId} />
-      <Schedules />
+      <Nav setItem={setItem}  cinemaId={cinemeId} setCinemaId={setCinemaId} />
+      <Movie item={item} setItem={setItem}  cinemaId={cinemeId} setSchedules={setSchedules} setSession={setSessions} />
+      <Schedule schedules={schedules}  />
+      <Session sessions={sessions} setSessions={setSessions} schedules={schedules} />
     </div>
   )
 }
